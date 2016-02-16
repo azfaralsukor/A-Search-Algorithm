a27f9afb54e838cbd867d83d498631981935342b
azfaralsukor <azfaralsukor@gmail.com>
2016-02-17 03:36:46 +0800

    fringe left!

--- A-Star-Search-Algorithm.lisp
+++ A-Star-Search-Algorithm.lisp
@@ -1,19 +1,20 @@
 ; A* Search Algorithm 
 
 (defclass City ()
-	((hn	   :accessor hn		  :initarg :hn		:initform 0)
-	 (name	   :accessor name	  :initarg :name 	:initform "Dummy")
-	 (f_s	   :accessor f_s	  :initarg :f_s 	:initform "")
-	 (f_gn	   :accessor f_gn	  :initarg :f_gn	:initform 0)
-	 (s_s	   :accessor s_s	  :initarg :s_s 	:initform "")
-	 (s_gn	   :accessor s_gn	  :initarg :s_gn	:initform 0)
-	 (t_s	   :accessor t_s	  :initarg :t_s 	:initform "")
-	 (t_gn	   :accessor t_gn	  :initarg :t_gn	:initform 0)
-	 (l_s	   :accessor l_s	  :initarg :l_s 	:initform "")
-	 (l_gn	   :accessor l_gn	  :initarg :l_gn	:initform 0)
+	((hn	   :accessor hn		  :initarg :hn		:initform 0)		;h(n)
+	 (name	   :accessor name	  :initarg :name 	:initform "Dummy")	;name
+	 (f_s	   :accessor f_s	  :initarg :f_s 	:initform "")		;first successor
+	 (f_gn	   :accessor f_gn	  :initarg :f_gn	:initform 0)		;first successor's g(n)
+	 (s_s	   :accessor s_s	  :initarg :s_s 	:initform "")		;second successor
+	 (s_gn	   :accessor s_gn	  :initarg :s_gn	:initform 0)		;second successor's g(n)
+	 (t_s	   :accessor t_s	  :initarg :t_s 	:initform "")		;third successor
+	 (t_gn	   :accessor t_gn	  :initarg :t_gn	:initform 0)		;third successor's g(n)
+	 (l_s	   :accessor l_s	  :initarg :l_s 	:initform "")		;last successor
+	 (l_gn	   :accessor l_gn	  :initarg :l_gn	:initform 0)		;;last successor's g(n)
 	)
 )
 
+;instantiations
 (setf A  (make-instance 'City :hn 366 :f_gn 140 :s_gn 118 :t_gn 75            :name "Arad"))
 (setf B  (make-instance 'City :hn   0 :f_gn 211 :s_gn 101 :t_gn  90 :l_gn  85 :name "Bucharest"))
 (setf C  (make-instance 'City :hn 160 :f_gn 120 :s_gn 138 :t_gn 146           :name "Craiova"))
@@ -28,13 +29,14 @@
 (setf N  (make-instance 'City :hn 234 :f_gn  87                               :name "Neamt"))
 (setf O  (make-instance 'City :hn 380 :f_gn  71 :s_gn 151                     :name "Oradea"))
 (setf P  (make-instance 'City :hn  98 :f_gn  97 :s_gn 138 :t_gn 101           :name "Pitesti"))
-(setf R  (make-instance 'City :hn 193 :f_gn  80 :s_gn  97 :t_gn 146           :name "Rimnicu Vilcea"))
+(setf R  (make-instance 'City :hn 193 :f_gn  80 :s_gn  97 :t_gn 146           :name "Ramnicu Valcea"))
 (setf S  (make-instance 'City :hn 253 :f_gn 151 :s_gn 140 :t_gn  99 :l_gn  80 :name "Sibiu"))
 (setf Ti (make-instance 'City :hn 329 :f_gn 118 :s_gn 111                     :name "Timisoara"))
 (setf U  (make-instance 'City :hn  80 :f_gn  85 :s_gn 142 :t_gn  98           :name "Urziceni"))
 (setf V  (make-instance 'City :hn 199 :f_gn  92 :s_gn 142                     :name "Vaslui"))
 (setf Z  (make-instance 'City :hn 374 :f_gn  71 :s_gn  75                     :name "Zerind"))
 
+;setting the successor seperately because the successor hasn't exist when it is declared to another instance's successor
 (setf (f_s A) S)
 (setf (s_s A) Ti)
 (setf (t_s A) Z)
@@ -103,35 +105,37 @@
 
 (format t "   A* Search Algorithm
 Initial State: Arad
-Goal         : Bucharest")
+Goal         : Bucharest~%")
 
 (setf gn 0)
 (setf fringe ())
 (setf travel "")
 
 (defun astar (current)
-	(setf travel (concatenate 'string travel (name current) " -> "))
+	(setf travel (concatenate 'string travel (name current) " -> ")) ;creating string of solution path
 	(setf new_city ())
 	(setf fn (+ (hn current) gn)) ; initial f(n) = g(n) + h(n)
 	(setf list_fn (cons fn ()))
 	(setf new_city (cons (name current) list_fn))
 	(setf fringe (cons new_city fringe))
-	(format t "~%~%Fringe : (")
+	; (format t (name current))
+	(format t "~%fringe (" (name current))
 	(setf fringe (sort fringe #'< :key #'second))
 	(dolist (i fringe) (format t " ~s "(car i))) 
-	(format t ")~%")
-	(format t (name current))
-	(format t "'s f(n) = " )
-	(format t "~a" fn) 
+	(format t ")")
+	(format t "~%Node Expanded : ~a ~%" (name current))
+	; (format t "'s f(n) = " )
+	; (format t "~a" fn) 
 
-	(format t "~%Next most minimum f(n) cost = " )
-	(format t "~a" (setf min_fn (min (if (= (f_gn current) 0) 9999 (+ (hn (f_s current)) (f_gn current))) 
+	; (format t "~%Next most minimum f(n) cost = " )
+	; (format t "~a" 
+			 	   (setf min_fn (min (if (= (f_gn current) 0) 9999 (+ (hn (f_s current)) (f_gn current))) 
 									 (if (= (s_gn current) 0) 9999 (+ (hn (s_s current)) (s_gn current))) 
 	  								 (if (= (t_gn current) 0) 9999 (+ (hn (t_s current)) (t_gn current)))
 	  								 (if (= (l_gn current) 0) 9999 (+ (hn (l_s current)) (l_gn current)))
 	  							)
 	  				)
-	)
+	; )
 
 	(cond   ((= min_fn (+ (hn (f_s current)) (f_gn current))) (setf next (f_s current)))
 		    ((= min_fn (+ (hn (s_s current)) (s_gn current))) (setf next (s_s current)))
@@ -146,14 +150,16 @@ Goal         : Bucharest")
 	)
 	
 	(setf fringe (cdr fringe))
-	(format t "~%Next = " )
-	(format t (name next))
-	(format t "~%Total Distance Traveled = ~a" gn )
-
-	(if (equal "Bucharest" (name next)) (format t "~%Solution Path: ~aBucharest"travel) (astar next))
+	; (format t "~%Next = " )
+	; (format t (name next))
+	
+	(if (equal "Bucharest" (name next)) 
+		(format t "~%Total Distance Traveled = ~a~%Solution Path: ~aBucharest"gn travel) 
+		(astar next)
+	)
 )
 
-(astar A)
+(astar A) ; the function starter
 
 ; (loop while (cond) do)
  
